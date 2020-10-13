61b73d967014b7c919a7d004cc351b7498a2b31c
Firoz Ansari <firozansari@stagecoach-technology.com>
2020-10-13 13:25:14 +0100

    Initial Commit

---
+++ RecipeAPI/RecipeAPI.h
@@ -0,0 +1,18 @@
+//
+//  RecipeAPI.h
+//  RecipeAPI
+//
+//  Created by Firoz Ansari on 13/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeAPI.
+FOUNDATION_EXPORT double RecipeAPIVersionNumber;
+
+//! Project version string for RecipeAPI.
+FOUNDATION_EXPORT const unsigned char RecipeAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeAPI/PublicHeader.h>
+
+
